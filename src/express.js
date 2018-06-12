6d90feabf98827a55970db3c0161173ad70b2055
Phuc-Dao <tidaotidao@yahoo.com>
2018-06-12 01:11:52 -0700

    addedc dependencies and middleware

--- src/express.js
+++ src/express.js
@@ -1,19 +1,39 @@
-
+var things = require('./things.js'); //require things from things.js
 const express = require('express');
-const app = express();
+bodyParser = require('body-parser');
+path = require('path');
+var app = express();
 
-/*These are various methods that we will work with in express
-app.get()
-app.post()
-app.put()
-app.delete
-*/
-//This the path to the home directory
-app.get('/', (req , res) => {
-   throw new Error("broken");
-    res.send("hello world"); 
+//use app.use to use the entire router. The path is /things
+//app.use('/', things);
+//Video ends at 23:20
+//MiddleWare functions are functions that executes before the get requests
+var logger = function(req , res , next){
+    console.log("The middleware has been activated");
+    next(); //The next function will execute the next middleware
+    
+}
+
+//second middleware function
+var logger2 = function(req , res , next){
+    console.log("The middleware number 2 hass been activated");
+    next(); //Always include the next function
+}
+
+//execute middle ware function by saying app.use. Executes bot these functions when its loaded
+app.use(logger , logger2);
+
+
+//Boiler plate code that renders things in the folder of public
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Hello world gets overwridden by whatever is in the html file
+app.get('/' , function(req , res){
+    res.send('Hello world'); 
+})
+
+app.listen(3000 , function(){
+    console.log('listening on port 3000...');
 });
-
-app.listen(3000) , function(){
-    console.log("The port is listening on 30000");
-};
